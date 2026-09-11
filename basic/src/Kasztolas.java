f2041e9835e918294919e8307a5ac0cb7ddc6561
K1nimod <164170790+K1nimod@users.noreply.github.com>
2026-09-11 12:14:50 +0200

    Add files via upload

---
+++ basic/src/Kasztolas.java
@@ -0,0 +1,73 @@
+public class Kasztolas {
+    static void main() {
+        String st = "string";
+        double d = 10.23;
+        float f = 200.23f;
+        long l = 200000000;
+        int i = 73;
+        char c = 'e';
+        short s = 4222;
+        byte b = 120;
+
+        //double->
+        /*System.out.println(d);
+        f  = (float) d;
+        System.out.println(f);
+        l = (long) f;
+        System.out.println(l);
+        i  = (int) l;
+        System.out.println(i);
+        c = (char) i;
+        System.out.println(c);
+        s = (short) c;
+        System.out.println(s);
+        b = (byte) s;
+        System.out.println(b);*/
+
+        //float->
+        /*System.out.println(f);
+        l = (long) f;
+        System.out.println(l);
+        i  = (int) l;
+        System.out.println(i);
+        c = (char) i;
+        System.out.println(c);
+        s = (short) c;
+        System.out.println(s);
+        b = (byte) s;
+        System.out.println(b);*/
+
+        //long->
+        /*System.out.println(l);
+        i  = (int) l;
+        System.out.println(i);
+        c = (char) i;
+        System.out.println(c);
+        s = (short) c;
+        System.out.println(s);
+        b = (byte) s;
+        System.out.println(b);*/
+
+        //int->
+        /*System.out.println(i);
+        c = (char) i;
+        System.out.println(c);
+        s = (short) c;
+        System.out.println(s);
+        b = (byte) s;
+        System.out.println(b);*/
+
+        //char->
+        /*System.out.println(c);
+        s = (short) c;
+        System.out.println(s);
+        b = (byte) s;
+        System.out.println(b);*/
+
+        //short-y
+        /*System.out.println(s);
+        b = (byte) s;
+        System.out.println(b);*/
+
+    }
+}
